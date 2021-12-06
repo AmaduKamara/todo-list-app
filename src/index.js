d8ec6a402ba9fa0164417052fe15c2941ff7e345
Amkam <amaduskamara@gmail.com>
2021-12-06 17:30:24 +0000

    Removed the id property from the Todo List object

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ const localStorageTodos = 'todo.lists';
 const todos = JSON.parse(localStorage.getItem(localStorageTodos)) || [];
 
 const createList = (name) => ({
-  id: Date.now().toString(),
   description: name,
   completed: false,
   index: todos.length + 1,
@@ -76,7 +75,9 @@ const handleRender = () => {
   document.querySelectorAll('.delete-todo').forEach((todo) => {
     todo.addEventListener('click', (e) => {
       const todos = JSON.parse(localStorage.getItem(localStorageTodos));
-      const filtered = todos.filter((todo) => todo.id !== e.target.parentNode.dataset.id);
+      const filtered = todos.filter(
+        (todo) => todo.id !== e.target.parentNode.dataset.id
+      );
       localStorage.setItem(localStorageTodos, JSON.stringify(filtered));
       e.target.closest('ul>li').remove();
     });
